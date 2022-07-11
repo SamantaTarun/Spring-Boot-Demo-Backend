633eb5aeb4d4f4a2af45705fb421a4eb38acf73e
Tarun Samanta <55488549+tarunsamanta2k20@users.noreply.github.com>
2022-07-12 01:20:28 +0530

    Update DepartmentRepository.java

--- src/main/java/com/example/full/springboot/tutorial/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/full/springboot/tutorial/demo/repository/DepartmentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
 
-    //vvi- video time: 1:23:55
+    
     Department findBydepartmentName(String departmentName);
 
     Department findBydepartmentNameIgnoreCase(String departmentName);
